abdae1bc8c297d90a6efac3524f4a7740bbc12c5
manji6 <ryosuke.sawada@gmail.com>
2024-09-22 18:09:30 +0900

    Corpサイト用のデータは別のリポジトリに移動したので削除

--- app/corpsite/header.tsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  return (
-    <header className="pb-5">
-      <div className="navbar bg-white shadow shadow-2xl opacity-95 rounded-lg">
-        <a className="btn btn-ghost text-xl">UG WORK</a>
-      </div>
-    </header>
-  )
-}
--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: "export",
   // github Pages の本番リリース時に独自ドメインを使う場合、ディレクトリが減るので意図的に減らす必要あり(package.json側も修正)
-  assetPrefix: process.env.NODE_ENV === "production" ? "/project_name" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "" : "",
 };
 
 export default nextConfig;
